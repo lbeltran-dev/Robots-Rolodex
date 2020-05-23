ba87c396e91b577748c366be739b4dc2b99b534e
Luis Alberto Beltrán Pérez <lbeltran@Ings-iMac.local>
2020-05-22 20:16:23 -0600

    Added project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter((robot) =>
+      robot.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder="Search Robots"
+          handleChange={this.handleChange}
+        />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+export default App;
